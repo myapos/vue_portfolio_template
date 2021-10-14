61b4cadfc9b078d5007d50a9fa87fa8ea6172053
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-10-14 12:47:34 +0300

    added handling for toggling mobile menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,45 @@
 <template>
-  <app-menu />
+  <app-mobile-menu v-if="showMobileMenu" />
+  <app-menu v-if="!showMobileMenu" />
 </template>
 
 <script>
 import AppMenu from "./Menu.vue";
+import AppMobileMenu from "./MobileMenu.vue";
+
 export default {
   name: "Header",
   components: {
     AppMenu,
+    AppMobileMenu,
   },
   props: {},
+  data() {
+    return {
+      width: this.getScreenWidth(),
+      mdBreakpoint: 768,
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", () => {
+      this.width = this.getScreenWidth();
+    });
+  },
+  computed: {
+    showMobileMenu: function () {
+      const displayMobileMenu = this.width < this.mdBreakpoint;
+
+      return displayMobileMenu;
+    },
+  },
+  methods: {
+    getScreenWidth() {
+      const screenWidth =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+      return screenWidth;
+    },
+  },
 };
 </script>
-
-<style scoped></style>
---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-800 p-4 flex justify-end">
+    <app-switch id="header" :disabled="false" />
+  </div>
+</template>
+
+<script>
+import AppSwitch from "../ui-kit/switch/Switch";
+export default {
+  name: "MobileMenu",
+  components: {
+    AppSwitch,
+  },
+};
+</script>
