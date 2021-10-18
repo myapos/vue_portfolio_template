e091c524834fb4b4cd568bf2905caccc2a30e913
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-10-18 23:42:13 +0300

    added gifs

---
+++ src/components/slider/Slider.vue
@@ -0,0 +1,30 @@
+<template>
+  <img v-if="activeTheme === 'dark'" src="../../assets/black_greet.gif" />
+  <img v-if="activeTheme === 'light'" src="../../assets/white_greet.gif" />
+</template>
+
+<script>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import "vue3-carousel/dist/carousel.css";
+export default {
+  name: "Slider",
+  components: {},
+  computed: {
+    activeTheme() {
+      return this.$store.getters.getActiveTheme();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.carousel {
+  height: 600px;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+img {
+  height: 500px;
+  width: 100%;
+}
+</style>
--- src/views/Base.vue
+++ src/views/Base.vue
@@ -1,5 +1,6 @@
 <template>
   <app-header />
+  <app-slider />
   <app-about />
   <app-footer />
 </template>
@@ -8,12 +9,15 @@
 import AppHeader from "../components/Header.vue";
 import AppFooter from "../components/Footer.vue";
 import AppAbout from "../components/About.vue";
+import AppSlider from "../components/slider/Slider.vue";
+
 export default {
   name: "Base",
   components: {
     AppHeader,
     AppFooter,
     AppAbout,
+    AppSlider,
   },
   props: {},
 };
