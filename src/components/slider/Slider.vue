9205bc914053fc1d305b02deef22fd97df3db562
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-10-19 01:02:16 +0300

    added static slide

--- src/components/slider/Slider.vue
+++ src/components/slider/Slider.vue
@@ -1,6 +1,52 @@
 <template>
-  <img v-if="activeTheme === 'dark'" src="../../assets/black_greet.gif" />
-  <img v-if="activeTheme === 'light'" src="../../assets/white_greet.gif" />
+  <div>
+    <img
+      class="relative"
+      v-if="activeTheme === 'dark'"
+      src="../../assets/black_greet.gif"
+    />
+    <img
+      class="relative"
+      v-if="activeTheme === 'light'"
+      src="../../assets/white_greet.gif"
+    />
+    <div
+      class="
+        details
+        text-center
+        ml-auto
+        mr-auto
+        left-0
+        right-0
+        dark:text-gray-50
+        z-0
+        absolute
+      "
+    >
+      <div class="intro_text text-4xl mb-10">We have some awesome things</div>
+      <div class="text-base mb-10">Let's make the web beautiful together</div>
+      <div
+        class="
+          ml-auto
+          mr-auto
+          mb-5
+          p-2
+          view_more
+          dark:hover:bg-gray-900
+          hover:bg-gray-100
+          cursor-pointer
+        "
+        :class="{ light_view_more: activeTheme === 'light' }"
+      >
+        View more
+      </div>
+      <div class="flex cursor-pointer justify-center">
+        <mdicon class="ml-2" name="RhombusOutline" />
+        <mdicon class="ml-2" name="RhombusOutline" />
+        <mdicon class="ml-2" name="RhombusOutline" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,13 +64,25 @@ export default {
 </script>
 
 <style scoped>
-.carousel {
-  height: 600px;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
 img {
-  height: 500px;
+  height: 600px;
   width: 100%;
+}
+
+.intro_text {
+  text-transform: uppercase;
+}
+.details {
+  z-index: 1;
+  top: 30%;
+  left: 0;
+  right: 0;
+}
+.view_more {
+  max-width: 150px;
+  border: 1px solid white;
+}
+.light_view_more {
+  border: 1px solid black;
 }
 </style>
