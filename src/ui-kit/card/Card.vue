aacb1e9e178572ac812f7eef97609f4e9f08178e
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-10-21 01:00:31 +0300

    basic approach

---
+++ src/ui-kit/card/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card flex flex-row">
+    <div class="m-5"><mdicon :name="icon" width="64" height="64" /></div>
+    <div class="flex flex-col m-5">
+      <div class="text-justify">{{ header }}</div>
+      <div class="text-justify">{{ text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    text: String,
+    header: String,
+    icon: String,
+  },
+};
+</script>
+
+<style></style>
