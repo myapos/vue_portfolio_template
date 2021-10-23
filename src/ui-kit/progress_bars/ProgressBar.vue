050c3b9aea40e2db6acd61f035125df91c15ddab
Myron Apostolakis <myapos@yahoo.com>
2021-10-23 19:30:41 +0300

    Skills (#4)
    * added skills section
    
    * styled skills section
    
    Co-authored-by: Myron Apostolakis <myron.apostolakis@sunlight.io>

---
+++ src/ui-kit/progress_bars/ProgressBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mt-5 mb-5">
+    <div class="font-bold">{{ title }}</div>
+    <div class="progress m-2">
+      <div class="progress_bar" :style="{ width: percentage }">
+        {{ percentage }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  props: {
+    title: String,
+    percentage: String,
+  },
+};
+</script>
+
+<style scoped>
+.progress {
+  border: 3px solid #363940;
+  background-color: transparent;
+  overflow: visible;
+  height: auto;
+  min-width: 840px;
+}
+
+.progress_bar {
+  background-color: #676d75;
+}
+
+/* On screens that are 600px or less, set the background color to olive */
+@media screen and (max-width: 840px) {
+  .progress {
+    min-width: 200px;
+  }
+}
+</style>
