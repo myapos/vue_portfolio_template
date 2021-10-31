24b57593425a17d8e61574d4389a550f2da0d9d1
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-11-01 01:08:10 +0200

    clean up

--- src/components/works/data.js
+++ src/components/works/data.js
@@ -12,21 +12,19 @@ export const categories = {
       title: "Night Abstract",
       subtitle: "Night Abstract",
       Demo: "link1",
-      background_image: require("../../assets/p4.jpg"),
-      // background_image:
-      //   "https://www.free-css.com/assets/files/free-css-templates/preview/page270/unika/assets/img/portfolio/p4.jpg",
+      background_image: require("@/assets/p4.jpg"),
     },
     {
       title: "Joy of independence",
       subtitle: "Subtitle 2",
       Demo: "link2",
-      background_image: require("../../assets/p5.jpg"),
+      background_image: require("@/assets/p5.jpg"),
     },
     {
       title: "Old is gold",
       subtitle: "Subtitle 3",
       Demo: "link3",
-      background_image: require("../../assets/p9.jpg"),
+      background_image: require("@/assets/p9.jpg"),
     },
   ],
   Webdesign: [
@@ -34,7 +32,7 @@ export const categories = {
       title: "Night Abstract",
       subtitle: "Night Abstract",
       Demo: "link1",
-      background_image: require("../../assets/p4.jpg"),
+      background_image: require("@/assets/p4.jpg"),
     },
   ],
   "App development": [
@@ -42,13 +40,13 @@ export const categories = {
       title: "Skull awesome",
       subtitle: "Sub 1",
       Demo: "link1",
-      background_image: require("../../assets/p1.jpg"),
+      background_image: require("@/assets/p1.jpg"),
     },
     {
       title: "Joy of independence",
       subtitle: "Subtitle 2",
       Demo: "link2",
-      background_image: require("../../assets/p5.jpg"),
+      background_image: require("@/assets/p5.jpg"),
     },
   ],
   "Graphics design": [
@@ -56,19 +54,19 @@ export const categories = {
       title: "Photo Frame",
       subtitle: "Sub 1",
       Demo: "link1",
-      background_image: require("../../assets/p2.jpg"),
+      background_image: require("@/assets/p2.jpg"),
     },
     {
       title: "Night crawlers",
       subtitle: "Subtitle 2",
       Demo: "link2",
-      background_image: require("../../assets/p6.jpg"),
+      background_image: require("@/assets/p6.jpg"),
     },
     {
       title: "Dirk road",
       subtitle: "Subtitle 2",
       Demo: "link2",
-      background_image: require("../../assets/p8.jpg"),
+      background_image: require("@/assets/p8.jpg"),
     },
   ],
   Responsive: [
@@ -76,13 +74,13 @@ export const categories = {
       title: "Hand Shots",
       subtitle: "Sub 1",
       Demo: "link1",
-      background_image: require("../../assets/p3.jpg"),
+      background_image: require("@/assets/p3.jpg"),
     },
     {
       title: "Last Motel",
       subtitle: "Sub 1",
       Demo: "link1",
-      background_image: require("../../assets/p7.jpg"),
+      background_image: require("@/assets/p7.jpg"),
     },
   ],
 };
