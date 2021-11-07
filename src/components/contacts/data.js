7de8f71bcd29548700632e5c3399a14878c075c4
Myron Apostolakis <myapos@yahoo.com>
2021-11-07 17:19:37 +0200

    completed address info (#18)
    * completed address info
    
    * completed contacts
    
    Co-authored-by: Myron Apostolakis <myron.apostolakis@sunlight.io>

---
+++ src/components/contacts/data.js
@@ -0,0 +1,14 @@
+export const data = {
+  info: {
+    street: "100 Limpbiscayne Blvd. (North) 17st Floor",
+    area: "New World Tower New York",
+    city: "New York",
+    country: "USA",
+    zip: "33148",
+    tel: "1 -234 -456 -7890",
+    fax: "1 -234 -456 -7890",
+    email: "info@yourdomain.com",
+    skype: "Iris-theme",
+  },
+  form_fields: [],
+};
